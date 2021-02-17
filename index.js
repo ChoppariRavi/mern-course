509b63a7bcaa971a4b02c60ba1688dfda9e37ee6
ChoppariRavi <choppari.ravi@gmail.com>
2021-02-17 16:13:24 +0530

    deployment requirement changes

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ app.get('/', (req, res) => {
     res.send({ 'message': 'Hello World!' })
 })
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => {
     console.log('App is running on port 5000!')
 })
